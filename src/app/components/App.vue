3da0c0a67fb8f03af88378fb8e3988da7b2f97a7
яна <AvramenkoYana@yandex.ru>
2019-04-23 22:07:25 +0300

    style change

--- src/app/components/App.vue
+++ src/app/components/App.vue
@@ -1,50 +1,47 @@
 <template>
     <div>
-        <nav class="navbar navbar-light bg-light">
-            <a href="/" class="navbar-brand">Markdown documents</a>
+        <nav class="navbar">
+            <span class="navbar-brand mb-0 h1">Markdown documents</span>
         </nav>
 
         <div class="container">
-            <div class="row pt-5">
-               <div class="col">
-                   <table class="table table-bordered">
-                       <thead>
-                        <tr>
-                            <th>Document</th>
-                        </tr>
-                       </thead>
-                       <tbody>
-                            <tr v-for="doc of documents">
-                                <td>{{doc.title}}</td>
-                                <td>
-                                    <button @click="deleteDocument(doc._id)" class="btn btn-danger">Delete</button>
-                                    <button @click="editDocument(doc._id)" class="btn btn-secondary">Edit</button>
-                                </td>
-                            </tr>
-                       </tbody>
-                   </table>
-               </div>
+            <div class="row">
+                <div class="col">
+                    <h4>Список документов</h4>
+                </div>
             </div>
-            <div class="row pt-5">
-                <div class="col-md-5">
-                    <div class="card">
-                        <div class="card-body">
-                            <form @submit.prevent="sendDocument">
-                                <div class="form-group">
-                                    <input type="text" v-model="document.title" placeholder="Document name" class="form-control">
-                                </div>
-                                <div class="form-group">
-                                    <textarea v-model="document.description" cols="30" rows="10" placeholder="Document content" class="form-control"></textarea>
-                                </div>
-                                <template v-if="edit === false">
-                                    <button class="btn btn-primary btn-block">Send</button>
-                                </template>
-                                <template v-else>
-                                    <button class="btn btn-primary btn-block">Update</button>
-                                </template>
-                            </form>
+            <div class="row">
+                <div class="col-md-auto pt-2">
+                    <ul class="list-group list-group-flush" v-for="doc of documents">
+                        <li class="list-group-item">
+                            <h5>{{doc.title}} &emsp;</h5>
+                            <button @click="deleteDocument(doc._id)">Удалить</button>
+                            <button @click="editDocument(doc._id)">Изменить</button>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+            <div class="row pt-4">
+                <div class="col">
+                    <h4>Добавление/изменение документа</h4>
+                </div>
+            </div>
+            <div class="row pt-4">
+                <div class="col-md-6">
+                    <form @submit.prevent="sendDocument">
+                        <div class="form-group">
+                            <input type="text" v-model="document.title" placeholder="Document name" class="form-control">
                         </div>
-                    </div>
+                        <div class="form-group">
+                            <textarea v-model="document.description" cols="30" rows="10" placeholder="Document content" class="form-control"></textarea>
+                        </div>
+                        <template v-if="edit === false">
+                            <button>Добавить</button>
+                        </template>
+                        <template v-else>
+                            <button>Обновить</button>
+                        </template>
+                    </form>
                 </div>
             </div>
         </div>
